e409a8f166d5cffaa5a84904125cb3fe47281cc5
tyuiu.pimenovaTS@outlook.com <Tatyana.filia.2005.pimenova@mail.ru>
2023-12-08 12:17:31 +0500

    Task4

--- Tyuiu.PimenovaTS.Sprint6.Task4.V27/FormMain.Designer.cs
+++ Tyuiu.PimenovaTS.Sprint6.Task4.V27/FormMain.Designer.cs
@@ -30,17 +30,17 @@ namespace Tyuiu.PimenovaTS.Sprint6.Task4.V27
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.groupBoxTask_PTS = new System.Windows.Forms.GroupBox();
             this.textBoxTask_PTS = new System.Windows.Forms.TextBox();
             this.groupBoxInput_PTS = new System.Windows.Forms.GroupBox();
-            this.labelStart_PTS = new System.Windows.Forms.Label();
-            this.labelStop_PTS = new System.Windows.Forms.Label();
-            this.textBoxStart_PTS = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBoxStop_PTS = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxStart_PTS = new System.Windows.Forms.TextBox();
+            this.labelStop_PTS = new System.Windows.Forms.Label();
+            this.labelStart_PTS = new System.Windows.Forms.Label();
             this.buttonDone_PTS = new System.Windows.Forms.Button();
             this.buttonSave_PTS = new System.Windows.Forms.Button();
             this.buttonHelp_PTS = new System.Windows.Forms.Button();
@@ -88,14 +88,26 @@ namespace Tyuiu.PimenovaTS.Sprint6.Task4.V27
             this.groupBoxInput_PTS.TabStop = false;
             this.groupBoxInput_PTS.Text = "Ввод данных:";
             // 
-            // labelStart_PTS
+            // textBoxStop_PTS
             // 
-            this.labelStart_PTS.AutoSize = true;
-            this.labelStart_PTS.Location = new System.Drawing.Point(7, 22);
-            this.labelStart_PTS.Name = "labelStart_PTS";
-            this.labelStart_PTS.Size = new System.Drawing.Size(87, 17);
-            this.labelStart_PTS.TabIndex = 0;
-            this.labelStart_PTS.Text = "Старт шага:";
+            this.textBoxStop_PTS.Location = new System.Drawing.Point(124, 43);
+            this.textBoxStop_PTS.Name = "textBoxStop_PTS";
+            this.textBoxStop_PTS.Size = new System.Drawing.Size(100, 22);
+            this.textBoxStop_PTS.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBoxStart_PTS
+            // 
+            this.textBoxStart_PTS.Location = new System.Drawing.Point(10, 43);
+            this.textBoxStart_PTS.Name = "textBoxStart_PTS";
+            this.textBoxStart_PTS.Size = new System.Drawing.Size(100, 22);
+            this.textBoxStart_PTS.TabIndex = 1;
             // 
             // labelStop_PTS
             // 
@@ -106,26 +118,14 @@ namespace Tyuiu.PimenovaTS.Sprint6.Task4.V27
             this.labelStop_PTS.TabIndex = 0;
             this.labelStop_PTS.Text = "Конец шага:";
             // 
-            // textBoxStart_PTS
+            // labelStart_PTS
             // 
-            this.textBoxStart_PTS.Location = new System.Drawing.Point(10, 43);
-            this.textBoxStart_PTS.Name = "textBoxStart_PTS";
-            this.textBoxStart_PTS.Size = new System.Drawing.Size(100, 22);
-            this.textBoxStart_PTS.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(116, 43);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBoxStop_PTS
-            // 
-            this.textBoxStop_PTS.Location = new System.Drawing.Point(124, 43);
-            this.textBoxStop_PTS.Name = "textBoxStop_PTS";
-            this.textBoxStop_PTS.Size = new System.Drawing.Size(100, 22);
-            this.textBoxStop_PTS.TabIndex = 1;
+            this.labelStart_PTS.AutoSize = true;
+            this.labelStart_PTS.Location = new System.Drawing.Point(7, 22);
+            this.labelStart_PTS.Name = "labelStart_PTS";
+            this.labelStart_PTS.Size = new System.Drawing.Size(87, 17);
+            this.labelStart_PTS.TabIndex = 0;
+            this.labelStart_PTS.Text = "Старт шага:";
             // 
             // buttonDone_PTS
             // 
@@ -180,22 +180,22 @@ namespace Tyuiu.PimenovaTS.Sprint6.Task4.V27
             // 
             // chartFunction_PTS
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chartFunction_PTS.ChartAreas.Add(chartArea4);
+            chartArea1.Name = "ChartArea1";
+            this.chartFunction_PTS.ChartAreas.Add(chartArea1);
             this.chartFunction_PTS.Location = new System.Drawing.Point(284, 102);
             this.chartFunction_PTS.Name = "chartFunction_PTS";
             this.chartFunction_PTS.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series4.Name = "Series1";
-            this.chartFunction_PTS.Series.Add(series4);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Name = "Series1";
+            this.chartFunction_PTS.Series.Add(series1);
             this.chartFunction_PTS.Size = new System.Drawing.Size(831, 388);
             this.chartFunction_PTS.TabIndex = 4;
-            title4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            title4.ForeColor = System.Drawing.Color.Blue;
-            title4.Name = "Title1";
-            title4.Text = "График функции (cos(x)/x+1)-cos(x)*1.3+3*x";
-            this.chartFunction_PTS.Titles.Add(title4);
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title1.ForeColor = System.Drawing.Color.Blue;
+            title1.Name = "Title1";
+            title1.Text = "График функции (cos(x)/x+1)-cos(x)*1.3+3*x";
+            this.chartFunction_PTS.Titles.Add(title1);
             // 
             // FormMain
             // 
@@ -213,7 +213,7 @@ namespace Tyuiu.PimenovaTS.Sprint6.Task4.V27
             this.MaximizeBox = false;
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Спринт 6 | Таск 4 | Вариант 27 | Пименова Т. С.";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.groupBoxTask_PTS.ResumeLayout(false);
             this.groupBoxTask_PTS.PerformLayout();
